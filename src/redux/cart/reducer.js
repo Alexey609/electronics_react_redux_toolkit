49c7825e70ede6fdc9e274b3d92c42e72d32b129
Alexey609 <alexxx609@mail.ru>
2021-11-24 20:34:52 +0500

    remove

--- src/redux/cart/reducer.js
+++ src/redux/cart/reducer.js
@@ -9,9 +9,6 @@ const cartSlice = createSlice({
         setItemInCart: (state, action) => {
            state.itemsInCart.push(action.payload)
         },
-        plusCartItem: (state, action) => {
-            
-        },
         deleteItem: (state, action) => {
             state.itemsInCart = state.itemsInCart.filter(good => good.id !== action.payload)
         }
